783528418557aeee8cba25de52c63f617d6af33d
sri-gudakesa2703 <srikar2985969c@gmail.com>
2023-01-30 23:39:32 +0530

    Qualification and Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,42 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+    tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove("qualification__active");
+        });
+        target.classList.add("qualification__active");
+
+        tabs.forEach((tab) => {
+            tab.classList.remove("qualification__active");
+        });
+        tab.classList.add("qualification__active");
+    });
+});
 
 /*==================== SERVICES MODAL ====================*/
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
